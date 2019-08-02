9d4e0f8c48878b0be5ac56f5fe043904518e4d86
Timothée Bilodeau <timothee.bilodeau@gmail.com>
2019-08-02 18:06:03 +0200

    Initial Commit

---
+++ MagikMeteo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagikMeteo
+//
+//  Created by Timothée Bilodeau on 02/08/2019.
+//  Copyright © 2019 Timothée Bilodeau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
